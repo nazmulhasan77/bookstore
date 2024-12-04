9ea06738eda5d08c129236c9e33517088f97d34f
Md Nazmul Hasan <nazmul7762@gmail.com>
2024-12-04 23:29:31 +0600

    Application run successfully

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('page-content')
+    <br>
+    <br>
+    <p class="text-center text-danger">
+        <i class="bi bi-book" style="font-size: 6rem;"></i>
+    </p>
+    <h1 class="text-center text-success">Laravel Book Store</h1>
+@endsection
